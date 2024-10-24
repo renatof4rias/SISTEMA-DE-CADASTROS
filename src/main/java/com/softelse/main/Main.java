c5d3be7ea807ae9d46f3e1f66ba226e012db765a
renatof4rias <renatof4rias@gmail.com>
2024-10-24 17:43:26 -0300

    feat: add ListAll

--- src/main/java/com/softelse/main/Main.java
+++ src/main/java/com/softelse/main/Main.java
@@ -1,16 +1,30 @@
 package com.softelse.main;
 
-import com.softelse.file.LeitorArquivo;
+import com.softelse.service.PessoaService;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LeitorArquivo leitor = new LeitorArquivo();
-        try {
-            leitor.LendoPerguntas();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        PessoaService leitor = new PessoaService();
+
+        Scanner sc = new Scanner(System.in);
+        int g = sc.nextInt();
+
+        while(!(g==0)){
+
+            try {
+                leitor.cadastrarPessoa();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
         }
+
+
+
+
+        leitor.listAllPessoas();
     }
 }
