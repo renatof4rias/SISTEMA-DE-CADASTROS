55048c868f3d9e9331228ccc156382d470ab2c29
renatof4rias <renatof4rias@gmail.com>
2024-10-29 18:44:12 -0300

    feat: Passo 6 - Procurar usuario por cadeia de carecter

--- src/main/java/com/softelse/service/PessoaService.java
+++ src/main/java/com/softelse/service/PessoaService.java
@@ -40,7 +40,6 @@ public class PessoaService {
 
     int i = 0;
     ArrayList<String> perguntasExtras;
-
     private void gerandoArquivo(Pessoa pessoa) {
         i++;
         perguntasExtras = cadastrarPergunta();
@@ -68,31 +67,86 @@ public class PessoaService {
     }
 
     int j = 0;
-
     public void listAllPessoas() {
-        j++;
         for (Pessoa Pessoa : listaPessoas) {
+            j++;
             System.out.println(j + " - " + Pessoa.getNome().toUpperCase());
         }
     }
 
-    public void buscarPessoaId() {
-        System.out.println("buscarPessoaId sem Implementação");
-        //trocar nome do metodo
+    public void buscarPessoa(){
+
+
+        j= 0;
+        System.out.print("Digite o Nome: ");
+        String nomeUsuario = respostaIn.nextLine();
+
+            for (Pessoa usuario : listaPessoas) {
+
+                if (usuario.getNome().toLowerCase().contains(nomeUsuario.toLowerCase())) {
+                    System.out.println(j + " - " +usuario.getNome());
+                }
+            }
+        System.out.println("Conseguio Encontrar o Usuário S/N ?");
+        String inPerguntas = respostaIn.nextLine();
+
+            while(true){
+                if(inPerguntas.equalsIgnoreCase("n")){
+                    System.out.print("Digite o Nome: ");
+                    nomeUsuario = respostaIn.nextLine();
+
+                    for (Pessoa usuario : listaPessoas) {
+                        if (usuario.getNome().toLowerCase().contains(nomeUsuario.toLowerCase())) {
+                            System.out.println(usuario.getNome());
+                        }
+                    }
+                } else if (inPerguntas.equalsIgnoreCase("s")) {
+                    System.out.println("Qual o Número do Usuário?");
+                    int numeroUsuario = respostaIn.nextInt();
+
+                    try {
+                        File file = new File("C:\\Users\\Renato\\Desktop\\SISTEMA-DE-CADASTROS\\DB\\" + String.valueOf((numeroUsuario + 1)) + "-" + pessoa.getNome().toUpperCase() + ".txt");
+                        FileReader fr = new FileReader(file);
+                        BufferedReader br = new BufferedReader(fr);
+                        System.out.println("\n");
+                        System.out.println("1 - Nome - " +listaPessoas.get(numeroUsuario).getNome());
+                        System.out.println("2 - Email - " +listaPessoas.get(numeroUsuario).getEmail());
+                        System.out.println("3 - Idade - " +listaPessoas.get(numeroUsuario).getIdade());
+                        System.out.println("4 - Altura - " +listaPessoas.get(numeroUsuario).getAltura());
+
+                        int numeroDaLinha = 0;
+                        String linha;
+                        while ((linha = br.readLine()) != null) {
+                            numeroDaLinha++;
+                            if (numeroDaLinha > 4) {
+                                System.out.println(linha + " ");
+                            }
+                        }
+                        break;
+                    }catch (IOException e){
+                        e.getMessage();
+                    }
+                }else {
+                    System.out.println("*** ERROR AO ESCOLHER ***");
+                    System.out.println("Conseguio Encontrar o Usuário S/N ?");
+                    inPerguntas = respostaIn.nextLine();
+                }
+            }
     }
 
+
     ArrayList<String> perguntaExtra;
-
-
     public ArrayList<String> cadastrarPergunta() {
         perguntaExtra = new ArrayList<>();
         int k = 4;
         System.out.print("\nDeseja Adicionar Perguntas Extras S/N ? ");
         String inPerguntas = respostaIn.nextLine();
+        boolean inPerguntasN = false;
         while (true) {
             if (inPerguntas.equalsIgnoreCase("n")) {
                 break;
             } else if (inPerguntas.equalsIgnoreCase("s")) {
+                inPerguntasN = true;
                 k++;
                 System.out.println("Digite sua Pergunta!");
                 String perguntaExtraIn = respostaIn.nextLine();
@@ -102,51 +156,58 @@ public class PessoaService {
                 inPerguntas = respostaIn.nextLine();
                 if (inPerguntas.equalsIgnoreCase("n")) {
                     break;
-                } else {
-                    System.out.println("*** ERROR AO ESCOLHER ***");
                 }
             } else {
                 System.out.println("*** ERROR AO ESCOLHER ***");
+                System.out.print("\nDeseja Adicionar Perguntas Extras S/N ? ");
+                inPerguntas = respostaIn.nextLine();
             }
         }
-
+        if (inPerguntas.equalsIgnoreCase("s")) {
         System.out.println("\nLista de Perguntas Registradas");
         for (String perguntaExtras : perguntaExtra) {
             System.out.println(perguntaExtras);
         }
-
-        System.out.print("Deseja Remover Alguma Pergunta S/N ? ");
-        inPerguntas = respostaIn.nextLine();
-        while (true) {
-            if (inPerguntas.equalsIgnoreCase("n")) {
-                break;
-            } else if (inPerguntas.equalsIgnoreCase("s")) {
-                System.out.print("Digite o Número da Perguta ?");
-                int perguntaExtraIn = respostaIn.nextInt();
-                respostaIn.nextLine();
-
-                System.out.println(perguntaExtra.get((perguntaExtraIn - 5)));
-
-                System.out.print("Deseja Remover Esta Pergunta S/N ?");
-                inPerguntas = respostaIn.nextLine();
+        }
+        if (inPerguntasN) {
+            System.out.print("Deseja Remover Alguma Pergunta S/N ? ");
+            inPerguntas = respostaIn.nextLine();
+            while (true) {
                 if (inPerguntas.equalsIgnoreCase("n")) {
                     break;
                 } else if (inPerguntas.equalsIgnoreCase("s")) {
-                    perguntaExtra.remove(perguntaExtraIn - 5);
+                    System.out.print("Digite o Número da Perguta ?");
+                    int perguntaExtraIn = respostaIn.nextInt();
+                    respostaIn.nextLine();
+
+                    System.out.println(perguntaExtra.get((perguntaExtraIn - 5)));
+
+                    System.out.print("Deseja Remover Esta Pergunta S/N ?");
+                    inPerguntas = respostaIn.nextLine();
+                    if (inPerguntas.equalsIgnoreCase("n")) {
+                        break;
+                    } else if (inPerguntas.equalsIgnoreCase("s")) {
+                        perguntaExtra.remove(perguntaExtraIn - 5);
+                    } else {
+                        System.out.println("*** ERROR AO ESCOLHER ***");
+                        System.out.print("Deseja Remover Esta Pergunta S/N ?");
+                        inPerguntas = respostaIn.nextLine();
+                    }
+                    System.out.println("Lista Atualizada de Perguntas Registradas");
+                    for (String perguntaExtras : perguntaExtra) {
+                        System.out.println(perguntaExtras);
+                    }
+
+                    System.out.print("Deseja Remover Outra Pergunta S/N ? ");
+                    inPerguntas = respostaIn.nextLine();
+
                 } else {
                     System.out.println("*** ERROR AO ESCOLHER ***");
+                    System.out.print("Deseja Remover Alguma Pergunta S/N ? ");
+                    inPerguntas = respostaIn.nextLine();
                 }
 
-                System.out.println("Lista Atualizada de Perguntas Registradas");
-                for (String perguntaExtras : perguntaExtra) {
-                    System.out.println(perguntaExtras);
-                }
-            } else {
-                System.out.println("*** ERROR AO ESCOLHER ***");
             }
-
-            System.out.print("Deseja Remover Outra Pergunta S/N ? ");
-            inPerguntas = respostaIn.nextLine();
         }
         return perguntaExtra;
     }
