5abf8d6e73a24524d9018bccc16dc32afa6dccee
renatof4rias <renatof4rias@gmail.com>
2024-10-30 17:38:54 -0300

    feat: Adicionando Idade Exception

---
+++ src/main/java/com/softelse/exeption/IdadeException.java
@@ -0,0 +1,10 @@
+package com.softelse.exeption;
+
+public class IdadeException extends RuntimeException{
+    public IdadeException() {
+    }
+
+    public IdadeException(String message) {
+        super(message);
+    }
+}
